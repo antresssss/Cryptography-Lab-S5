34ae20d6984b0af6884700a9315517a5f02eec6a
antresssss <andreatresa2004@gmail.com>
2024-10-21 00:11:26 +0530

    first program

---
+++ XorWith0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+char str[20]="Hello World";
+char resxor[20]=" ";
+
+
+for(int i=0;i<strlen(str);i++){
+	resxor[i]=str[i]^0;	
+}
+printf("result of xor operation:%s \n",resxor);
+}
